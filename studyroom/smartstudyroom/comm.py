bc7210b91426bc1187ca2fbd925511403fb7c861
Kim Ga Young <bunny0098@naver.com>
2020-06-25 07:42:28 +0900

    error 수정

--- studyroom/smartstudyroom/comm.py
+++ studyroom/smartstudyroom/comm.py
@@ -4,7 +4,7 @@ import threading
 def transmit(ip):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    sock.connect(ip, 10007)
+    sock.connect((ip, 10007))
     data = sock.recv(65535)
     d = data.decode('utf-8')
     print(data.decode('utf-8'))
